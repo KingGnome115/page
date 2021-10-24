4828690fd347ab763fb19760f8e65eb397037358
KingGnome115 <kevinjec115@gmail.com>
2021-10-24 15:15:26 -0500

    Cree un menubar para viajar entre las paginas

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,21 @@
 <template>
+  <nav-bar></nav-bar>
   <router-view></router-view>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-
+import NavBar from './components/NavBar.vue'
 export default defineComponent({
   name: 'App',
   components: {
+    NavBar,
   }
 });
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,20 @@
+<template>
+    <nav>
+        <ul>
+            <li>
+                <router-link to="/">Tareas</router-link>
+            </li>
+            <li>
+                <router-link to="/tasks/new">Nueva Tarea</router-link>
+            </li>
+        </ul>
+    </nav>
+</template>
+
+<script lang="ts">
+    import { defineComponent } from '@vue/runtime-core'
+
+    export default defineComponent({
+        name:"Navbar"
+    })
+</script>
