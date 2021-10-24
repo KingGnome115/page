9b136846c0ce7ffd78015660967dafdce9ad5ade
KingGnome115 <kevinjec115@gmail.com>
2021-10-23 22:12:35 -0500

    Listo para iniciar en blanco

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <p>Sigo vivo</p>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
 
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
   }
 });
 </script>
