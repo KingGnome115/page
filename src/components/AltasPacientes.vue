e120a9601cffcd69254fb6c8280eeeee358fa8aa
Reyno21 <gerardoyaz21@gmail.com>
2021-10-26 15:02:06 -0500

    Creo que ya quedo, es lo que pude hacer

--- src/components/AltasPacientes.vue
+++ src/components/AltasPacientes.vue
@@ -5,13 +5,13 @@
         </h4>
         <!------->
         <form class="row g-3" action="REGISTROPACIENTE.html" method="get" id="contenedor">
-            <div class="col-mid-1 pnomap">
-                <div class="col-md-3 nomap">
+            <div class="col-mid-6 pnomap">
+                <div class="col-md-4 nomap">
                     <label for="Name" class="form-label"> Nombre de pila</label>
                     <input type="text" class="form-control" id="nombre" >
                 </div>   
 
-                <div class="col-md-3 nomap">
+                <div class="col-md-4 nomap">
                     <label for="apellido" class="form-label"> Primer apellido</label>
                     <input type="text" class="form-control" id="PrimeApellido" >
                 </div>
@@ -23,7 +23,7 @@
             </div>
 
             <div>
-                <div class="col-md-3 cin">
+                <div class="col-md-6 cin">
                 <label for="name">CURP:</label>
                 <input type="text" id="name" name="paciente" class="form-control">
                 </div>
@@ -38,17 +38,19 @@
                     <input type="number" id="name" name="paciente" class="form-control">
                 </div>
             </div>
-
             <div>
-    <label for="fechaNac">Fecha Nacimiento</label>
-                <li>
-                    <input type="date" id="start" name="trip-start"
-                    value="2018-07-22"
-                    min="1920-01-01" max="2023-12-31">
-                </li>
+                <br>
+            </div>
+
+            <div >
+                <div class="col">
+                    <legend class="col-form-label col-sm-2 pt-0">Fecha Nacimiento</legend>
+                    <input type="date" id="start" name="trip-start" value="2018-07-22" min="1920-01-01" max="2023-12-31">
+
+                </div>                
 
                 <div class="col-md-4">
-                <label for="estadoCi">Estado Civil</label>
+                <legend class="col-form-label col-sm-1 pt-0">Estado Civil</legend>
                     <select name="ECivil" size="1"> 
                         <option value="default" selected class="form-control"> 
                             --- seleccione su estado civil --- 
@@ -67,9 +69,9 @@
                         </option> 
                     </select> 
                 </div>
-
+                         
                 <div class="col-md-4">
-                    <label for="estadoCi">Genero</label>
+                    <legend class="col-form-label col-sm-1 pt-0">Genero</legend>
                         <select name="genero" size="1"> 
                             <option value="default" selected> 
                                     --- sleccione su genero--- 
@@ -87,52 +89,56 @@
                 </div>
             </div>
             
-            
-            <div>
-                <div class="col-md-2">
+            <div class="col-md-9 nomap cin">
+
+                <div class="col-md-4 cin nomap">
                     <label for="name">RAZA :</label>
                     <input type="text" id="name" name="paciente" class="form-control">
+                    
                 </div>
-            
-                <div class="col-md-2">
+
+                <div class="col-md-4 cin nomap">
                     <label for="name">ETNIA :</label>
                     <input type="text" id="name" name="paciente" class="form-control">
                 </div>
+                
+            
+                <div class="col-md-3 cin">
+                    <label for="Escolaridad"> Escolaridad</label>
+                    <select name="escolaridad" size="1"> 
+                    <option value="default" selected> 
+                        --- sleccione--- 
+                    </option> 
+                    <option value="Primaria"> 
+                        PRIMARIA
+                    </option> 
+                    <option value="SECUNDARIA"> 
+                    SECUNDARIA
+                    </option> 
+                    <option value="PREPARATORIA"> 
+                    PREPARATORIA
+                    </option> 
+                    <option value="UNIVERSIDAD"> 
+                        UNIVERSIDAD
+                    </option> 
+                    </select> 
+                </div>
+
             </div>
             
-            <div class="col-md-4">
-                <label for="Escolaridad"> Escolaridad</label>
-                <select name="escolaridad" size="1"> 
-                <option value="default" selected> 
-                    --- sleccione--- 
-                </option> 
-                <option value="Primaria"> 
-                    PRIMARIA
-                </option> 
-                <option value="SECUNDARIA"> 
-                SECUNDARIA
-                </option> 
-                <option value="PREPARATORIA"> 
-                PREPARATORIA
-                </option> 
-                <option value="UNIVERSIDAD"> 
-                    UNIVERSIDAD
-                </option> 
-                </select> 
-            </div>
 
-            <div class="col-md-4">
-                <div class="col-md-4">
+            <div>
+                <div class="col-md-3 nomap">
                 <label for="telefono">Ocupación </label>
                 <input type="text" id="ocupacion" name="Ocuppaciente" class="form-control">
                 </div>
                 
-                <div class="col-md-4">
+                <div class="col-md-3 nomap">
                 <label for="telefono">Telefono:</label>
                 <input type="number" id="tel" size="10" name="Telfpaciente" class="form-control">
                 </div>
                 
-                <div class="col-md-4">
+                <div class="col-md-5 nomap">
                 <label for="name">Domicilio:</label>
                 <input type="text" id="name" name="paciente" class="form-control">
                 </div>
@@ -160,7 +166,9 @@
             </div>
             
             <div class="col-md-4">
-                <p>Contacto referencia</p>
+                <br>
+                <legend class="col-form-label col-sm-0 pt-0">Contacto de referencia</legend>
+
                 <label for="name">Nombre de pila :</label>
                 <input type="text" id="name" name="paciente" class="form-control">
                 
@@ -188,8 +196,8 @@
 
 <script lang="ts">
     import {defineComponent} from 'vue'
-    import {Paciente} from '@/interfaces/Paciente'
-    import {agregarPaciente} from '@/services/PacienteServices'
+    import {Paciente} from '../interfaces/Paciente'
+    import {agregarPaciente} from '../services/PacienteServices'
 
     export default defineComponent({
         data(){
--- src/components/MEPacientes.vue
+++ src/components/MEPacientes.vue
@@ -1,17 +1,90 @@
 <template lang="">
-    <div>
-        <h1>Detalles de tareas</h1>
+
+<div>
+     <h1>Detalles de Pacientes</h1>
+
+        <!--   List   -->
+    <div class="col-md-7">
+        <table class="table table-bordered table-hover">
+             <thead>
+                 <tr>
+                    <th>Id</th>
+                    <th>Nombre pila</th>
+                    <th>Primer Apellido</th>
+                    <th>Segundo Apellido</th>
+                    <th>CURP</th>
+                    <th>Iniciales</th>
+                    <th>No. Identificacion</th>
+                    <th>Fecha Nacimiento</th>
+                    <th>Estado civil</th> <!-- checar como funciona estas opciones creo-->
+                    <th>Genero</th> 
+                    <th>Raza</th>
+                    <th>Etnia</th>
+                    <th>Escolaridad</th>
+                    <th>Ocupación</th>
+                    <th>Telefono</th>
+                    <th>Doicilio</th>
+                    <th>Fecha realizado</th>
+                    <th>Lugar origen</th>
+                    <th>Hospital referencia</th>
+                    <th>Operaciones</th>
+
+                        <!--
+                        <th>Contacto referencia</th>
+                        <th>Nombre de pila</th>
+                        <th>Primer apellido</th>
+                        <th>Segundo apellido</th>
+                        <th>Parentesco</th>
+                        -->
+                        
+                </tr>
+            </thead>
+            <tbody>
+                <!-- aqui es donde va el task
+                    <% for (var i= 0; i<task.length; i++){ %>
+                        <tr>
+                            <td> <%= task[i]._id %></td>
+                            <td> <%= task[i].nomPila %></td>
+                            <td> <%= task[i].primApellido %></td>
+                            <td> <%= task[i].secApellido %></td>
+                            <td> <%= task[i].curp %></td>
+                            <td> <%= task[i].iniciales %></td>
+                            <td> <%= task[i].nIdentificacion %></td>
+                            <td> <%= task[i].fechaNacimiento %></td>
+                            ...
+                            <td>
+                                <a href="/edit/ <td> <%= task[i]._id %></td>" class="btn btn-info">Editar</a>
+                                 <a href="/delete/ <td> <%= task[i]._id %></td>" class="btn btn-danger">Eliminar</a>
+
+                            </td>
+
+                      
+                        </tr>
+                        } 
+                    
+                    
+                    -->
+                <tr></tr>
+            </tbody>
+        </table>
+
+    </div>
+
+
+        <!-- <h1>Detalles de tareas</h1>
         <form @submit.prevent="handleUpdate()">
             <input type="text" v-model="pacienteActual.titulo">
             <textarea rows="3" v-model="pacienteActual.descripcion"></textarea>
             <button type="">Actualizar</button>
         </form>
+        
         <button @click="handleDelete()">Eliminar</button>
-    </div>
+        -->
+</div>
 </template>
 <script lang="ts">
-    import { Paciente } from "@/interfaces/Paciente";
-    import { consultarPaciente, modificarPaciente, eliminarPaciente } from "@/services/PacienteServices";
+    import { Paciente } from "../interfaces/Paciente";
+    import { consultarPaciente, modificarPaciente, eliminarPaciente } from "../services/PacienteServices";
     import { defineComponent } from "@vue/runtime-core";    
     export default defineComponent({
         data(){
