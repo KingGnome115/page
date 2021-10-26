8cc2ef790beb90224bde2da41dc7a42bc8eae85c
KingGnome115 <kevinjec115@gmail.com>
2021-10-26 02:05:12 -0500

    Ya lee los datos de los inputs

--- src/components/AltaProtocolo.vue
+++ src/components/AltaProtocolo.vue
@@ -3,90 +3,80 @@
         <h4 align="center" id="titulo">
             FORMULARIO ALTA DE PROTOCOLOS
         </h4>
-        <form class="row g-3" action="Protocolo.html" method="get" id="contenedor" @sumbit.prevent="guardarProtocolo()">
+        <form class="row g-3" @submit.prevent="guardarProtocolo()">
             <div class="col-md-6">
-                <label for="nombreProtocolo" class="form-label"> Nombre del nuevo protocolo: </label>
-                <input type="text" class="form-control" id="nombreProtocolo" placeholder="Ej. Protocolo 1 " v-model="protocolo.nomProtocolo">
+                <label class="form-label"> Nombre del nuevo protocolo: </label>
+                <input type="text" class="form-control" placeholder="Ej. Protocolo 1" v-model="protocolo.nomProtocolo">
             </div>
 
             <div class="col-md-3">
-                <label for="numeroProtocolo" class="form-label"> Numero de protocolo: </label>
-                <input type="text" class="form-control" id="numeroProtocolo" placeholder="Ej. 10 " v-model="protocolo.numeroProtocolo">
+                <label class="form-label"> Numero de protocolo: </label>
+                <input type="text" class="form-control"  placeholder="Ej. 10" v-model.number="protocolo.numeroProtocolo">
             </div>
 
             <div class="col-md-3">
                 <label for="numeroVisitas" class="form-label"> Numero de visitas totales: </label>
-                <input type="number" class="form-control" id="numeroVisitas" size="10" placeholder="Ej. 15 visitas" min="1" v-model="protocolo.numeroVisitas">
+                <input type="number" class="form-control" size="10" placeholder="Ej. 15 visitas" min="1" v-model.number="protocolo.numeroVisitas">
             </div>
 
             <div class="col-md-3">
                 <label for="tipoNomenclatura" class="form-label"> Tipo de Nomenclatura: </label>
-                <input type="text" class="form-control" id="tipoNomenclatura" placeholder="Ej. V1,V2,...Vn " v-model="protocolo.nomeclatura">
+                <input type="text" class="form-control" placeholder="Ej. V1,V2,...Vn " v-model="protocolo.nomeclatura">
             </div>
 
             <!--Radio Buttons Periodo-->
             <legend class="col-form-label col-sm-1 pt-0">Tipo de Periodo:</legend>
             <div class="col-md-2">
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="tipoPeriodo" id="periodo1" value="option1" checked v-model="protocolo.nomeclatura">
-                    <label class="form-check-label" for="periodo1">
-                        Dias
-                    </label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="radio" name="tipoPeriodo" id="periodo2" value="option2">
-                    <label class="form-check-label" for="periodo2">
-                        Semanas
-                    </label>
+                    <select v-model="protocolo.tipoDePeriodo">
+                        <option value="Dia" selected >Dia</option>
+                        <option value="Semana">Semana</option>
+                    </select>
                 </div>
             </div>
                 
             <div class="col-md-3">
-                <label for="tamPeriodo" class="form-label"> Tamaño del periodo: </label>
-                <input type="number" class="form-control" id="tamPeriodo" min="1" placeholder="Ej. 3 dias/semanas" v-model="protocolo.tamPeriodo">
+                <label class="form-label"> Tamaño del periodo: </label>
+                <input type="number" class="form-control" min="1" placeholder="Ej. 3 dias/semanas" v-model.number="protocolo.tamanioPeriodo">
             </div>
 
             <div class="col-md-3">
-                <label for="visita0" class="form-label"> Visita 0: </label>
-                <input type="number" class="form-control" id="visita0" min="2" placeholder="Ej. 3ra vista" v-model="protocolo.visita0">
+                <label class="form-label"> Visita 0: </label>
+                <input type="number" class="form-control" min="2" placeholder="Ej. 3ra vista" v-model.number="protocolo.visitaCero">
             </div>
 
             <div class="col-md-4">
-                <label for="numViSemAnterior" class="form-label"> Numero de semanas que contiene cada visita anterior:</label>
-                <input type="number" class="form-control" id="numViSemAnterior" min="1" placeholder="Ej. 5 semanas" v-model="protocolo.numViSemAnterior">
+                <label class="form-label"> Numero de semanas que contiene cada visita anterior:</label>
+                <input type="number" class="form-control" min="1" placeholder="Ej. 5 semanas" v-model.number="protocolo.numeroSemanas" >
             </div>
 
             <div class="col-md-4">
-                <label for="ventanaAntes" class="form-label">Dias (Antes) de ventana en cada cita/visita:</label>
-                <input type="number" class="form-control" id="ventanaAntes" min="0" max="3" placeholder="Ej. 2 dias/semanas" v-model="protocolo.ventanaAntes">
+                <label class="form-label">Dias (Antes) de ventana en cada cita/visita:</label>
+                <input type="number" class="form-control" min="0" max="3" placeholder="Ej. 2 dias/semanas" v-model.number="protocolo.diasACC" >
             </div>
 
             <div class="col-md-4">
-                <label for="ventanaDespues" class="form-label">Dias (Despues) de ventana en cada cita/visita:</label>
-                <input type="number" class="form-control" id="ventanaDespues" min="0" max="3" placeholder="Ej. 2 dias/semanas" v-model="protocolo.ventanaDespues">
+                <label class="form-label">Dias (Despues) de ventana en cada cita/visita:</label>
+                <input type="number" class="form-control" min="0" max="3" placeholder="Ej. 2 dias/semanas" v-model.number="protocolo.DCC" >
             </div>
 
             <div class="col-md-4">
-                <label for="visitaEOS" class="form-label">Visita EOS (Fin de Estudio):</label> 
-                <input type="number" class="form-control" id="visitaEOS" min="2" placeholder="Ej. 3ra vista" v-model="protocolo.visitaEOS">
+                <label class="form-label">Visita EOS (Fin de Estudio):</label> 
+                <input type="text" class="form-control" min="2" placeholder="Ej. 3ra vista" v-model="protocolo.eotEstudio" >
             </div>
 
             <div class="col-md-4">
-                <label for="visitaEOT" class="form-label">Visita EOT (Fin de Tratamiento):</label> 
-                <input type="number" class="form-control" id="visitaEOT" min="2" placeholder="Ej. 3ra vista" v-model="protocolo.visitaEOT">
+                <label class="form-label">Visita EOT (Fin de Tratamiento):</label> 
+                <input type="text" class="form-control" min="2" placeholder="Ej. 3ra vista" v-model="protocolo.eotTratamiento" >
             </div>
 
             <div  class="col-md-4">
-                <label for="color" class="form-label"> Color de identificacion de protocolo: </label> 
-                <input type="color" class="form-control form-control-color" value="#0d6efd" id="muestrario" v-model="protocolo.color"> <br>
+                <label class="form-label"> Color de identificacion de protocolo: </label> 
+                <input type="color" class="form-control form-control-color" v-model="protocolo.color"> <br>
             </div>
 
             <div class="col-auto" style="text-align: right;width:1235px">
                 <button type="submit" class="btn btn-primary mb-4">Guardar</button>
-            </div>
-
-            <div class="col-auto">
-                <button type="reset" class="btn btn-primary mb-4">Cancelar</button>
             </div>
         </form>
     </div>
@@ -96,18 +86,32 @@
 import {defineComponent} from 'vue'
 import {Protocolo} from '@/interfaces/Protocolos'
 import { agregarProtocolo } from '@/services/ProtocoloServices'
-import { model } from 'mongoose';
 
 export default defineComponent({
     data() {
         return{
-            protocolo: {} as Protocolo
+            protocolo:{
+                nomProtocolo:"" as string,
+                numeroProtocolo: 0 as number,
+                numeroVisitas: 0 as number,
+                nomeclatura:"" as string,
+                tipoDePeriodo:"" as string,
+                tamanioPeriodo: 0 as number,
+                visitaCero: 0 as number,
+                numeroSemanas: 0 as number,
+                diasACC: 0 as number,
+                diasDCC: 0 as number,
+                eotEstudio:"" as string,
+                eotTratamiento:"" as string,
+                color:"" as string,
+            }
         }
     },
     methods:{
         async guardarProtocolo(){
-            const res = await agregarProtocolo(this.protocolo)
-            this.$router.push('/')
+            //const res = await agregarProtocolo(this.protocolo)
+            console.log(this.protocolo)
+            //this.$router.push('/')
         }
 
     }
