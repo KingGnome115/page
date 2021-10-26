30a40b7bffe8ce9383c8cf46102ade9738447b90
KingGnome115 <kevinjec115@gmail.com>
2021-10-26 14:55:51 -0500

    Ya muestra los pasientes y los elimina

--- src/components/MEPacientes.vue
+++ src/components/MEPacientes.vue
@@ -1,14 +1,47 @@
 <template lang="">
-    <div>
-        <h1>Detalles de tareas</h1>
-        <form @submit.prevent="handleUpdate()">
-            <input type="text" v-model="pacienteActual.titulo">
-            <textarea rows="3" v-model="pacienteActual.descripcion"></textarea>
-            <button type="">Actualizar</button>
-        </form>
-        <button @click="handleDelete()">Eliminar</button>
+    <div class="Contenido"> 
+        <h1>Detalles de Pacientes</h1>
+        <div class="col-md-12 ">
+            <table class="table table-bordered table-hover">
+                <thead>
+                    <th>Nombre pila</th>
+                    <th>CURP</th>
+                    <th>No. Identificacion</th>
+                    <th>Genero</th> 
+                    <th>Raza</th>
+                    <th>Etnia</th>
+                    <th>Telefono</th>
+                    <th>Domicilio</th>
+                    <th>Fecha realizado</th>
+                    <th>Lugar origen</th>
+                    <th>Hospital referencia</th>
+                </thead>
+                <tr>
+                    <th> {{pacienteActual.nomPila}} 
+                         {{pacienteActual.primApellido}}
+                         {{pacienteActual.segApellido}} </th>
+                    <th> {{pacienteActual.curp}} </th>
+                    <th> {{pacienteActual.noIdent}} </th>
+                    <th> {{pacienteActual.genero}} </th>
+                    <th> {{pacienteActual.raza}} </th>
+                    <th> {{pacienteActual.etnia}} </th>
+                    <th> {{pacienteActual.telefono}} </th>
+                    <th> {{pacienteActual.domicilio}} </th>
+                    <th> {{pacienteActual.fechaRea}} </th>
+                    <th> {{pacienteActual.lugarOrigen}} </th>
+                    <th> {{pacienteActual.hospitalRef}} </th>
+                </tr>
+            </table>
+
+            <div class="botones">
+                <button class="btn btn-primary mb-4" type="" @click="handleDelete()">Eliminar</button>
+                <button class="btn btn-primary mb-4" type="">Modificar</button>
+            </div>
+
+        </div>
     </div>
 </template>
+
 <script lang="ts">
     import { Paciente } from "@/interfaces/Paciente";
     import { consultarPaciente, modificarPaciente, eliminarPaciente } from "@/services/PacienteServices";
@@ -40,6 +73,37 @@
         }
     })
 </script>
+
 <style scoped>
-    
+    .Contenido{
+        padding: 10px;
+        background: #a7f19b;
+    }
+
+    table, th, tr, td{
+        padding: 5px;
+        width: 100%;
+        border: 2px solid #4F7849;
+        text-align: center;
+        border-collapse: collapse;
+    }
+
+    .Ope{
+        padding: 10px;
+        width: 110px;
+    }
+
+    .botones{
+        margin: auto;
+        justify-content: center;
+        text-align: center;
+    }
+
+    button{
+        margin: 10px;
+    }
+
+    thead{
+        color: #4F7849;
+    }
 </style>
