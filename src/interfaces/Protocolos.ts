66822758ef3eb87e42dac498c06e62d51acf160c
KingGnome115 <kevinjec115@gmail.com>
2021-11-09 13:02:37 -0600

    Dividi la interfas protocolo en dos protocolo y visitas y al primero le di un arreglod e visitas

--- src/interfaces/Protocolos.ts
+++ src/interfaces/Protocolos.ts
@@ -1,16 +1,10 @@
+import {Visitas} from '../interfaces/visitas'
+
 export interface Protocolo{
     nomProtocolo:string;
     numeroProtocolo:number;
     numeroVisitas:number;
-    nomeclatura:string;
-    tipoDePeriodo:string;
-    tamanioPeriodo:number;
-    visitaCero:number;
-    numeroSemanas:number;
-    diasACC:number;
-    diasDCC:number;
-    eotEstudio:string;
-    eotTratamiento:string;
+    visitas:Visitas[];
     color:string;
     _id:string;
 }
---
+++ src/interfaces/Visitas.ts
@@ -0,0 +1,10 @@
+export interface Visitas {
+    nomeclatura: string;
+    tipoDePeriodo: string;
+    tamanioPeriodo: number;
+    visitaCero: boolean;
+    numeroSemanas: number;
+    dias: number;
+    eotEstudio: boolean;
+    eotTratamiento: boolean;
+}
