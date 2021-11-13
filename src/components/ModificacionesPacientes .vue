1e791bbbb9f6eac9a71a731c62a75a6bd2503820
DanielaGF02 <danielagf02@hotmail.com>
2021-11-12 19:16:25 -0600

    altapacientes corregida e interfaz modicarpacientes realizada

--- src/components/ModificacionesPacientes .vue
+++ src/components/ModificacionesPacientes .vue
@@ -1,41 +1,41 @@
 <template lang="">
     <div class="Contenido">
         <h4 align="center" id="titulo">
-            REGISTRO DE PACIENTES
+            MODIFICACIONESDE PACIENTES
         </h4>
-        <!------->
+        <!---->
         <form class="row g-3" @submit.prevent="guardarPaciente()">
             <div class="col-mid-1 pnomap">
                 <div class="col-md-3 nomap">
                     <label for="Name" class="form-label"> Nombre de pila<abbr title="required" aria-label="required">*</abbr></label>
-                    <input type="text" class="form-control" v-model="paciente.nomPila" >
+                    <input type="text" class="form-control" v-model="paciente.nomPila" readonly onmousedown="return false;" >
                 </div>   
 
                 <div class="col-md-4 nomap">
                     <label for="apellido" class="form-label"> Primer apellido<abbr title="required" aria-label="required">*</abbr></label>
-                    <input type="text" class="form-control" v-model="paciente.primApellido" >
+                    <input type="text" class="form-control" v-model="paciente.primApellido" readonly onmousedown="return false;" >
                 </div>
 
                 <div class="col-md-3 nomap">
                     <label for="apellido2" class="form-laabel">Segundo apellido<abbr title="required" aria-label="required">*</abbr></label>
-                    <input type="text" class="form-control" v-model="paciente.segApellido" >
+                    <input type="text" class="form-control" v-model="paciente.segApellido" readonly onmousedown="return false;" >
                 </div>
             </div>
 
             <div>
                 <div class="col-md-6 cin">
                 <label for="name">CURP<abbr title="required" aria-label="required">*</abbr></label>
-                <input type="text" class="form-control" v-model="paciente.curp">
+                <input type="text" class="form-control" v-model="paciente.curp" readonly onmousedown="return false;">
                 </div>
 
                 <div class="col-md-2 cin">
                     <label for="inicial">Iniciales <abbr title="required" aria-label="required">*</abbr></label>
-                    <input type="text" class="form-control" v-model="paciente.iniciales">
+                    <input type="text" class="form-control" v-model="paciente.iniciales" readonly onmousedown="return false;">
                 </div>
 
                 <div class="col-md-3 cin">
                     <label for="name">No.Identificacion<abbr title="required" aria-label="required">*</abbr>label>
-                    <input type="number" class="form-control" v-model="paciente.noIdent">
+                    <input type="number" class="form-control" v-model="paciente.noIdent" readonly onmousedown="return false;">
                 </div>
             </div>
             <div>
@@ -43,7 +43,7 @@
                 <li>
                     <input type="date"
                     min="1920-01-01" max="2023-12-31"
-                    v-model="paciente.fechaNac">
+                    v-model="paciente.fechaNac" readonly onmousedown="return false;" >
                 </li>
 
                 <div class="col-md-4">
@@ -90,12 +90,12 @@
 
                 <div class="col-md-4 cin nomap">
                     <label for="name">RAZA<abbr title="required" aria-label="required">*</abbr></label>
-                    <input type="text" class="form-control" v-model="paciente.raza">
+                    <input type="text" class="form-control" v-model="paciente.raza" readonly onmousedown="return false;">
                 </div>
 
                 <div class="col-md-4 cin nomap">
                     <label for="name">ETNIA<abbr title="required" aria-label="required">*</abbr></label>
-                    <input type="text" class="form-control" v-model="paciente.etnia">
+                    <input type="text" class="form-control" v-model="paciente.etnia" readonly onmousedown="return false;">
                 </div>
                 
             </div>
@@ -124,17 +124,17 @@
             <div>
                 <div class="col-md-3 nomap">
                 <label for="telefono">Ocupación<abbr title="required" aria-label="required">*</abbr></label>
-                <input type="text" class="form-control" v-model="paciente.ocupacion">
+                <input type="text" class="form-control" v-model="paciente.ocupacion" readonly onmousedown="return false;">
                 </div>
                 
                 <div class="col-md-3 nomap">
                 <label for="telefono">Telefono<abbr title="required" aria-label="required">*</abbr></label>
-                <input type="number" size="10" class="form-control" v-model="paciente.telefono">
+                <input type="number" size="10" class="form-control" v-model="paciente.telefono" readonly onmousedown="return false;">
                 </div>
                 
                 <div class="col-md-5 nomap">
                 <label for="name">Domicilio<abbr title="required" aria-label="required">*</abbr></label>
-                <input type="text" class="form-control" v-model="paciente.domicilio">
+                <input type="text" class="form-control" v-model="paciente.domicilio" readonly onmousedown="return false;">
                 </div>
             </div>
 
@@ -143,17 +143,17 @@
                 <div  class="col-md-4">
                     <label for="fechaR">Fecha realizado<abbr title="required" aria-label="required">*</abbr></label>
                 <input type="date"
-                            min="1920-01-01" max="2023-12-31" v-model="paciente.fechaRea">
+                            min="1920-01-01" max="2023-12-31" v-model="paciente.fechaRea" readonly onmousedown="return false;">
                 </div>
                 
                 <div class="col-md-4">
                     <label for="name">Lugar origen<abbr title="required" aria-label="required">*</abbr></label>
-                    <input type="text" class="form-control" v-model="paciente.lugarOrigen">
+                    <input type="text" class="form-control" v-model="paciente.lugarOrigen" readonly onmousedown="return false;">
                 </div>
             
                 <div class="col-md-4">
                     <label for="name">Hospital referencia<abbr title="required" aria-label="required">*</abbr></label>
-                    <input type="text" class="form-control" v-model="paciente.hospitalRef">
+                    <input type="text" class="form-control" v-model="paciente.hospitalRef" readonly onmousedown="return false;">
                 </div>
             </div>
 
@@ -164,23 +164,23 @@
                 <legend class="col-form-label col-sm-0 pt-0">Contacto de referencia</legend>
 
                 <label for="name">Nombre de pila<abbr title="required" aria-label="required">*</abbr></label>
-                <input type="text" class="form-control" v-model="paciente.nomPilaCon">
+                <input type="text" class="form-control" v-model="paciente.nomPilaCon" readonly onmousedown="return false;">
                 
                 <label for="apellido">Primer apellido<abbr title="required" aria-label="required">*</abbr></label>
-                <input type="text" class="form-control" v-model="paciente.primApellidoCon">
+                <input type="text" class="form-control" v-model="paciente.primApellidoCon" readonly onmousedown="return false;">
 
                 <label for="apellido2">Segundo apellido<abbr title="required" aria-label="required">*</abbr></label>
-                <input type="text" class="form-control" v-model="paciente.segApellidoCon">
+                <input type="text" class="form-control" v-model="paciente.segApellidoCon" readonly onmousedown="return false;">
 
                 <label for="name">Parentesco<abbr title="required" aria-label="required">*</abbr></label>
-                <input type="text" class="form-control" v-model="paciente.parentesco">
+                <input type="text" class="form-control" v-model="paciente.parentesco" readonly onmousedown="return false;">
 
                 <label for="name">Telefono<abbr title="required" aria-label="required">*</abbr></label>
-                <input type="text" class="form-control" v-model="paciente.telefonoCon">
+                <input type="text" class="form-control" v-model="paciente.telefonoCon" readonly onmousedown="return false;">
             </div>
 
             <div class="col-auto" style="text-align: right;width:1235px">
-                <button type="submit" class="btn btn-primary mb-4">Guardar</button>
+                <button type="submit" class="btn btn-primary mb-4">Guardar y modificar </button>
             </div>
 
             <div class="col-auto">
