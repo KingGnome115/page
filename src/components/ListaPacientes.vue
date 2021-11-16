73edefc5b43dfabd629e8b779ef355a6effbb1b2
KingGnome115 <kevinjec115@gmail.com>
2021-11-16 14:07:52 -0600

    Ya hay una barra de busqueda para lista de pacientes

--- src/components/ListaPacientes.vue
+++ src/components/ListaPacientes.vue
@@ -1,7 +1,17 @@
 <template lang="">
     <div class="Contenido">
-        <h1>Lista de pacientes</h1>
-        <ul>
+        <h1 class="ti">Lista de pacientes</h1>
+        
+        <div class="field has-addons is-pulled-right">
+            <div class="control">
+                <input v-model="nombre" type="text" class="form-control valid barraBusqueda" placeholder="Nombre del paciente" v-on:keyup.enter="buscarData">
+            </div>
+            <div class="control">
+                <button class="btn btn-primary"  v-on:click="buscarData">Buscar</button>
+            </div>
+        </div>
+
+        <ul class="lista" >
             <li v-for="(paciente, index) in pacientes" :key="index" @click="this.$router.push(`/pacientes/${paciente._id}`)">
                 {{paciente.nomPila}} {{paciente.primApellido}} {{paciente.segApellido}} <hr>
             </li>
@@ -13,22 +23,30 @@
     import { consultarPacientes } from '../services/PacienteServices'
     import {defineComponent} from 'vue'
     import { Paciente } from '../interfaces/Paciente';
-
     export default defineComponent({
         data(){
             return{
-                pacientes: [] as Paciente[]
+                pacientes: [] as Paciente[],
+                nombre: ''
             }
         },
         methods:{
             async cargarPacientes(){
                 const res = await consultarPacientes()
                 this.pacientes = res.data
+            },
+            async buscarData(){
+                this.pacientes.forEach(paciente => {
+                    let nombreCompleto = paciente.nomPila + ' ' + paciente.primApellido + ' ' + paciente.segApellido
+                    if(nombreCompleto.indexOf(this.nombre) > -1){
+                        this.$router.push(`/pacientes/${paciente._id}`)
+                    }
+                })
             }
         },
         mounted(){
             this.cargarPacientes()
-        }
+        },
     })
 
 </script>
@@ -40,4 +58,23 @@
     .Contenido{
         padding: 10px;
     }
+
+    .ti{
+        font-size: 30px;
+        font-weight: bold;
+        text-align: center;
+    }
+    
+    .barraBusqueda{
+        float: right;
+        width: 30%;
+        margin: .5em 2em 0 0;
+    }
+
+    button{
+        float: right;
+        width: 15%;
+        margin: 3.5em 2em;
+    }
+
 </style>
