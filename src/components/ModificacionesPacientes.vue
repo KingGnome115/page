f39d7623b7d47c424517aa0b35ad4141fd0113df
NaaraiGasca <snai2016@gmail.com>
2021-11-26 19:02:15 -0600

    Modificaciones de la ventana de modificaciones de pacientes

--- src/components/ModificacionesPacientes.vue
+++ src/components/ModificacionesPacientes.vue
@@ -9,57 +9,36 @@
                 <div class="col-12 col-md-4 mb-3">
                     <label for="Name" class="form-label"> Nombre de pila </label>
                     <input type="text" class="form-control valid" id="nombre" placeholder="Ej. Juan" required v-model="paciente.nomPila">
-                    <div class="invalid-feedback">
-                        Por favor escriba un nombre
-                    </div>
                 </div> 
 
                 <div class="col-12 col-md-4 mb-3">
                     <label for="apellido" class="form-label"> Primer apellido </label>
                     <input type="text" class="form-control valid" id="apellido" placeholder="Ej. Perez" required v-model="paciente.primApellido">
-                    <div class="invalid-feedback">
-                        Por favor escriba un apellido
-                    </div>
                 </div>
 
                 <div class="col-12 col-md-4 mb-3">
                     <label for="apellido2" class="form-label"> Segundo apellido </label>
                     <input type="text" class="form-control valid" id="apellido2" placeholder="Ej. Mendez" required v-model="paciente.segApellido">
-                    <div class="invalid-feedback">
-                        Por favor escriba un apellido
-                    </div>
                 </div>
                 
                 <div class="col-12 col-md-3 mb-3">
                     <label for="inicial" class="form-label">Iniciales </label>
                     <input type="text" class="form-control valid" id="inicial" placeholder="Ej. JPM" required v-model="paciente.iniciales">
-                    <div class="invalid-feedback">
-                        Por favor escriba las iniciales
-                    </div>
                 </div>
 
                 <div class="col-12 col-md-6 mb-3">
                     <label for="curp" class="form-label">CURP</label>
                     <input type="text" class="form-control valid" id="curp" placeholder="Ej. PEMJ060914HHCJULA5" required v-model="paciente.curp">
-                    <div class="invalid-feedback">
-                        Por favor escriba el CURP
-                    </div>
                 </div>
 
                 <div class="col-12 col-md-3 mb-3">
                     <label for="numid" class="form-label">No.Identificacion</label>
                     <input type="number" min="1" class="form-control valid" id="numid" placeholder="Ej. 10" required v-model="paciente.noIdent">
-                    <div class="invalid-feedback">
-                        Por favor escriba el numero de identificacion 
-                    </div>
                 </div>
                 
                 <div class="col-12 col-md-3 mb-3">
                     <label for="fechaNac" class="form-label">Fecha Nacimiento</label>
                     <input type="date" min="1920-01-01" max="2023-12-31" class="form-control valid" id="fechaNac" required v-model="paciente.fechaNac">
-                    <div class="invalid-feedback">
-                        Por favor ingrese su fecha de nacimiento
-                    </div>
                 </div>
 
                 <!--Estado civil-->
@@ -72,9 +51,6 @@
                         <option>Divorciado</option>
                         <option>Viudo</option>
                     </select>
-                    <div id="ECivil" class="invalid-feedback">
-                        Por favor, seleccione una opcion
-                    </div>
                 </div>
 
                 <!--Genero-->
@@ -86,25 +62,16 @@
                         <option>Mujer</option>
                         <option>No definido</option>
                     </select>
-                    <div id="genero" class="invalid-feedback">
-                        Por favor, seleccione una opcion
-                    </div>
                 </div>
 
                 <div class="col-12 col-md-3 mb-3">
                     <label for="raza" class="form-label">Raza </label>
                     <input type="text" class="form-control valid" id="raza" placeholder="" required v-model="paciente.raza">
-                    <div class="invalid-feedback">
-                        Por favor escriba la raza
-                    </div>
                 </div>
 
                 <div class="col-12 col-md-3 mb-3">
                     <label for="etnia" class="form-label">Etnia</label>
                     <input type="text" class="form-control valid" id="etnia" placeholder="" required v-model="paciente.etnia">
-                    <div class="invalid-feedback">
-                        Por favor escriba la etnia
-                    </div>
                 </div>
 
                 <!--Escolaridad--> 
@@ -117,57 +84,36 @@
                         <option>Preparatoria</option>
                         <option>Universidad</option>
                     </select>
-                    <div id="escolaridad" class="invalid-feedback">
-                        Por favor, seleccione una opcion
-                    </div>
                 </div>
 
                 <div class="col-12 col-md-3 mb-3">
                     <label for="ocupacion" class="form-label">Ocupación</label>
                     <input type="text" class="form-control valid" id="ocupacion" placeholder="Ej. Obrero" required v-model="paciente.ocupacion">
-                    <div class="invalid-feedback">
-                        Por favor ingrese la ocupacion
-                    </div>
                 </div>
                     
                 <div class="col-12 col-md-3 mb-3">
                     <label for="telefono" class="form-label">Telefono</label>
                     <input type="number" size="10" class="form-control valid" id="telefono" placeholder="Ej. 7226041050" required v-model="paciente.telefono">
-                    <div class="invalid-feedback">
-                        Por favor ingrese el telefono
-                    </div>
                 </div>
                     
                 <div class="col-12 mb-3">
                     <label for="domicilio" class="form-label">Domicilio</label>
                     <input type="text" class="form-control valid" id="domicilio" placeholder="Ej. Duraznos 19, Casa Blanca, Metepec, Estado de Mexico, Mexico" required v-model="paciente.domicilio">
-                    <div class="invalid-feedback">
-                        Por favor ingrese un domicilio valido
-                    </div>
                 </div>
 
                 <div  class="col-12 col-md-4 mb-3">
                     <label for="fechaR" class="form-label">Fecha realizado</label>
                     <input type="date" min="1920-01-01" max="2023-12-31" class="form-control valid" id="fechaR" required v-model="paciente.fechaRea">
-                    <div class="invalid-feedback">
-                        Por favor ingrese una fecha valida
-                    </div>
                 </div>
                     
                 <div class="col-12 col-md-4 mb-3">
                     <label for="lugarO" class="form-label">Lugar origen</label>
                     <input type="text" class="form-control valid" id="lugarO" placeholder="Ej. Morelia" required v-model="paciente.lugarOrigen">
-                    <div class="invalid-feedback">
-                        Por favor ingrese un lugar de origen
-                    </div>
                 </div>
                 
                 <div class="col-12 col-md-4 mb-3">
                     <label for="hospitalR" class="form-label">Hospital referencia</label>
                     <input type="text" class="form-control valid" id="hospitalR" placeholder="Ej. Monica Pretelini" required v-model="paciente.hospitalRef">
-                    <div class="invalid-feedback">
-                        Por favor ingrese un hospital de referencia
-                    </div>
                 </div>
 
                 <hr>
@@ -178,41 +124,26 @@
                 <div class="col-12 col-md-4 mb-3">
                     <label for="nameCon" class="form-label">Nombre de pila</label>
                     <input type="text" class="form-control valid" id="nameCon" placeholder="Ej. Maria" required v-model="paciente.nomPilaCon">
-                    <div class="invalid-feedback">
-                        Por favor ingrese un nombre de contacto
-                    </div>
                 </div>
 
                 <div class="col-12 col-md-4 mb-3">
                     <label for="apellidoCon" class="form-label">Primer apellido</label>
                     <input type="text" class="form-control valid" id="apellidoCon" placeholder="Ej. Dominguez" required v-model="paciente.primApellidoCon">
-                    <div class="invalid-feedback">
-                        Por favor ingrese un apellido
-                    </div>
                 </div>
 
                 <div class="col-12 col-md-4 mb-3">
                     <label for="apellido2Con" class="form-label">Segundo apellido</label>
                     <input type="text" class="form-control valid" id="apellido2Con" placeholder="Ej. Mendez" required v-model="paciente.segApellidoCon">
-                    <div class="invalid-feedback">
-                        Por favor ingrese un apellido
-                    </div>
                 </div>
                     
                 <div class="col-12 col-md-4 mb-3">
                     <label for="parentescoCon" class="form-label">Parentesco</label>
                     <input type="text" class="form-control valid" id="parentescoCon" placeholder="Ej. Madre" required v-model="paciente.parentesco">
-                    <div class="invalid-feedback">
-                        Por favor ingrese el parentesco con el paciente
-                    </div>
                 </div>
 
                 <div class="col-12 col-md-4 mb-3">
                     <label for="telefonoCon" class="form-label">Telefono</label>
                     <input type="text" class="form-control valid" id="telefonoCon" placeholder="Ej. 7224865123" required v-model="paciente.telefonoCon">
-                    <div class="invalid-feedback">
-                        Por favor ingrese un numero de contacto
-                    </div>
                 </div>
 
                 <!--Botones-->
@@ -260,18 +191,13 @@
 </script>
 
 <style scoped>
-    .Contenido{
+    header {
+        background: var(--fondo-contenido);
+    }
+
+    body {
+        background: #F8F8FF;
         padding: 10px;
     }
 
-    div{
-        display: inline-block;
-        text-align: center;
-        margin: auto;
-    }
-
-    .nomap, .cin{
-        display: inline-block;
-        margin: 10px;
-    }
 </style>
