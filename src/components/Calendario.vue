754a533120d15dd3430571a4c213aed27c9e2ce6
KingGnome115 <kevinjec115@gmail.com>
2022-01-09 15:27:40 -0600

    Pequeños cambios

---
+++ src/components/Calendario.vue
@@ -0,0 +1,15 @@
+<template lang="">
+    <h1>Calendario</h1>
+    <div >
+    </div>
+</template>
+
+<script lang="ts">
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+    
+</style>
