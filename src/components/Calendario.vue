6690d9e0acae7a3caded0de81ba809b46050efd4
KingGnome115 <kevinjec115@gmail.com>
2022-01-30 17:00:08 -0600

    Barra de busqueda dinamica para buscar dependiendo si es protocolo o pacientes

--- src/components/Calendario.vue
+++ src/components/Calendario.vue
@@ -21,44 +21,34 @@
 						</div>
 					</div>
 				</div>
-
-
-				<div class="field">
-					<label class="label">Temas</label> <br>
-					<label class="checkbox">
-						<input v-model="useDefaultTheme" type="checkbox" />
-						Default
-					</label>
-				</div>
-
 			</div>
 
 			<div class="box">
 				<div class="field">
-					<h4>Crear evento</h4>
-					<label class="label">Nombre del evento</label>
-					<div class="control">
-						<input v-model="newItemTitle" class="input" type="text" />
-					</div>
+					<h4>Ver por</h4>
+					<select v-model="tipoBusqueda">
+						<option @click="cargarDatos" selected value="Pr">Protocolos</option>
+						<option @click="cargarDatos" value="Pa">Pacientes</option>
+						<option value="Dc">Doctores</option>
+					</select>
 				</div>
-
-				<div class="field">
-					<label class="label">Fecha de inicio de evento</label>
-					<div class="control">
-						<input v-model="newItemStartDate" class="input" type="date" />
-					</div>
-				</div>
-
-				<div class="field">
-					<label class="label">Fecha de fin de evento </label>
-					<div class="control">
-						<input v-model="newItemEndDate" class="input" type="date" />
-					</div>
-				</div>
-
-				<button class="button is-info" @click="clickTestAddItem">Agregar</button>
 			</div>
+
+			<div class="box">
+				<div class="field">
+					<h4>Busqueda</h4>
+					<div class="control">
+                    <input v-model="nombre" v-on:keypress="recon" type="text" autocomplete="off" v-bind="state" @input="filterStates" class="form-control valid barraBusqueda">
+					</div>
+				</div>
+			</div>
+			<div>
+                <ul class="list">
+                    <li v-for="(filt, index) in filteredStates" :key="index" @click="buscaData(filt)"> {{filt}} </li>
+                </ul>
+            </div>
 		</div>
+
 		<div class="calendar-parent">
 			<calendar-view
 				:items="items"
@@ -94,155 +84,232 @@
 	</div>
 </template>
 <script>
-// Load CSS from the published version
-import "vue-simple-calendar/dist/style.css"
-import "vue-simple-calendar/static/css/default.css"
-import "vue-simple-calendar/static/css/holidays-us.css"
-// Load CSS from the local repo
-//require("../../vue-simple-calendar/static/css/default.css")
-//require("../../vue-simple-calendar/static/css/holidays-us.css")
-import { CalendarView, CalendarViewHeader, CalendarMath } from "vue-simple-calendar" // published version
-//} from "../../vue-simple-calendar/src/components/bundle.js" // local repo
-export default {
-	name: "App",
-	components: {
-		CalendarView,
-		CalendarViewHeader,
-	},
-	data() {
-		return {
-			/* Show the current month, and give it some fake items to show */
-			showDate: this.thisMonth(1),
-			message: "",
-			startingDayOfWeek: 0,
-			disablePast: false,
-			disableFuture: false,
-			displayPeriodUom: "month",
-			displayPeriodCount: 1,
-			displayWeekNumbers: false,
-			showTimes: true,
-			selectionStart: null,
-			selectionEnd: null,
-			newItemTitle: "",
-			newItemStartDate: "",
-			newItemEndDate: "",
-			useDefaultTheme: true,
-			useHolidayTheme: true,
-			useTodayIcons: false,
-			items: [
-				{
-					id: "e0",
-					startDate: "2018-01-05",
-				},
-				{
-					id: "e2",//Sera el mismo que el de la base
-					startDate: this.thisMonth(10),//Aqui seleccionamos el dia (partiendo la fecha de cada cita)
-					title: "Evento de referencia",//Aqui Nombre paciente + Protocolo
-				},
-				{
-					id: "61d0bd0b4e6540fb07193b86",
-					startDate: this.thisMonth(16),//Aqui seleccionamos el dia
-					title: "Evento de color",
-					classes: "orange"
-				},
-				
-			],
-		}
-	},
-	computed: {
-		userLocale() {
-			return CalendarMath.getDefaultBrowserLocale
+	// Load CSS from the published version
+	import "vue-simple-calendar/dist/style.css"
+	import "vue-simple-calendar/static/css/default.css"
+	import "vue-simple-calendar/static/css/holidays-us.css"
+	// Load CSS from the local repo
+	//require("../../vue-simple-calendar/static/css/default.css")
+	//require("../../vue-simple-calendar/static/css/holidays-us.css")
+	import { CalendarView, CalendarViewHeader, CalendarMath } from "vue-simple-calendar" // published version
+	//} from "../../vue-simple-calendar/src/components/bundle.js" // local repo
+	//Para traer los protocolos
+	import { consultarProtocolosRex } from '../services/ProtocoloServices';
+    import { consultarProtocoloNom } from '../services/ProtocoloServices';
+    import { Protocolo } from '../interfaces/Protocolos';
+	//Para los pacientes
+	import { consultarPacienteNom } from '../services/PacienteServices'
+    import { consultarPacientesRex } from '../services/PacienteServices'
+	import { Paciente } from '../interfaces/Paciente';
+
+	export default {
+		name: "App",
+		components: {
+			CalendarView,
+			CalendarViewHeader,
 		},
-		dayNames() {
-			return CalendarMath.getFormattedWeekdayNames(this.userLocale, "long", 0)
-		},
-		themeClasses() {
+		data() {
 			return {
-				"theme-default": this.useDefaultTheme,
-				"holiday-us-traditional": this.useHolidayTheme,
-				"holiday-us-official": this.useHolidayTheme,
+				cadenaBusqueda: "",
+				tipoBusqueda: "Pr",
+				//Para buscar por protocolos
+				protocolos: [],
+                protocoloEnc: '',
+				//
+				//Para buscar por pacientes
+				pacientes: [],
+				pacienteEnc: '',
+				//
+				//Para la barra de busqueda
+				nombre: '',
+                state:'',
+                states: [],
+                filteredStates: [],
+				//
+				/* Show the current month, and give it some fake items to show */
+				showDate: this.thisMonth(1),
+				message: "",
+				startingDayOfWeek: 0,
+				disablePast: false,
+				disableFuture: false,
+				displayPeriodUom: "month",
+				displayPeriodCount: 1,
+				displayWeekNumbers: false,
+				showTimes: true,
+				selectionStart: null,
+				selectionEnd: null,
+				newItemTitle: "",
+				newItemStartDate: "",
+				newItemEndDate: "",
+				useDefaultTheme: true,
+				useHolidayTheme: true,
+				useTodayIcons: false,
+				items: [
+					{
+						id: "e0",
+						startDate: "2018-01-05",
+					},
+					{
+						id: "e2",//Sera el mismo que el de la base
+						startDate: this.thisMonth(10),//Aqui seleccionamos el dia (partiendo la fecha de cada cita)
+						title: "Evento de referencia",//Aqui Nombre paciente + Protocolo
+					},
+					{
+						id: "61d0bd0b4e6540fb07193b86",
+						startDate: this.thisMonth(16),//Aqui seleccionamos el dia
+						title: "Evento de color",
+						classes: "orange"
+					},
+					
+				],
 			}
 		},
-		myDateClasses() {
-			// This was added to demonstrate the dateClasses prop. Note in particular that the
-			// keys of the object are `yyyy-mm-dd` ISO date strings (not dates), and the values
-			// for those keys are strings or string arrays. Keep in mind that your CSS to style these
-			// may need to be fairly specific to make it override your theme's styles. See the
-			// CSS at the bottom of this component to see how these are styled.
-			const o = {}
-			const theFirst = this.thisMonth(1)
-			const ides = [2, 4, 6, 9].includes(theFirst.getMonth()) ? 15 : 13
-			const idesDate = this.thisMonth(ides)
-			o[CalendarMath.isoYearMonthDay(idesDate)] = "ides"
-			o[CalendarMath.isoYearMonthDay(this.thisMonth(21))] = ["do-you-remember", "the-21st"]
-			return o
-		},
-	},
-	mounted() {
-		//Aqui llamamos al metodo para generar items
-		this.newItemStartDate = CalendarMath.isoYearMonthDay(CalendarMath.today())
-		this.newItemEndDate = CalendarMath.isoYearMonthDay(CalendarMath.today())
-	},
-	methods: {
-		periodChanged() {
-			// range, eventSource) {
-			// Demo does nothing with this information, just including the method to demonstrate how
-			// you can listen for changes to the displayed range and react to them (by loading items, etc.)
-			//console.log(eventSource)
-			//console.log(range)
-		},
-		thisMonth(d, h, m) {
-			const t = new Date()
-			console.log("AQUI ESTAS LO QUE BUSCAS")
-			console.log(t.getFullYear())
-			console.log(t.getMonth())
-			console.log(d)
-			console.log(h)
-			console.log(0, m)
-			console.log(0)
-			console.log("AQUI ESTAS LO QUE BUSCAS")
-			return new Date(t.getFullYear(), t.getMonth(), d, h || 0, m || 0)
+		computed: {
 			
+			userLocale() {
+				return CalendarMath.getDefaultBrowserLocale
+			},
+			dayNames() {
+				return CalendarMath.getFormattedWeekdayNames(this.userLocale, "long", 0)
+			},
+			themeClasses() {
+				return {
+					"theme-default": this.useDefaultTheme,
+					"holiday-us-traditional": this.useHolidayTheme,
+					"holiday-us-official": this.useHolidayTheme,
+				}
+			},
+			myDateClasses() {
+				// This was added to demonstrate the dateClasses prop. Note in particular that the
+				// keys of the object are `yyyy-mm-dd` ISO date strings (not dates), and the values
+				// for those keys are strings or string arrays. Keep in mind that your CSS to style these
+				// may need to be fairly specific to make it override your theme's styles. See the
+				// CSS at the bottom of this component to see how these are styled.
+				const o = {}
+				const theFirst = this.thisMonth(1)
+				const ides = [2, 4, 6, 9].includes(theFirst.getMonth()) ? 15 : 13
+				const idesDate = this.thisMonth(ides)
+				o[CalendarMath.isoYearMonthDay(idesDate)] = "ides"
+				o[CalendarMath.isoYearMonthDay(this.thisMonth(21))] = ["do-you-remember", "the-21st"]
+				return o
+			},
 		},
-		onClickDay(d) {
-			this.selectionStart = null
-			this.selectionEnd = null
-			this.message = `Dia seleccionado: ${d.toLocaleDateString()}`
+		mounted() {
+			this.cargarDatos();
+			//Aqui llamamos al metodo para generar items
+			this.newItemStartDate = CalendarMath.isoYearMonthDay(CalendarMath.today())
+			this.newItemEndDate = CalendarMath.isoYearMonthDay(CalendarMath.today())
 		},
-		onClickItem(e) {
-			this.message = `Evento seleccionado: ${e.title}`
+		methods: {
+			async cargarDatos() {
+				this.states.length = 0;
+				this.filteredStates.length = 0;
+				if(this.tipoBusqueda == "Pr"){
+					const res = await consultarProtocolosRex('a');
+					this.protocolos = res.data;
+					this.protocolos.forEach(element => {
+						this.states.push(element);
+					});
+					console.log(this.states + " Protocolos");
+				} else if(this.tipoBusqueda == "Pa") {
+					const res = await consultarPacientesRex('a');
+					this.pacientes = res.data;
+					this.pacientes.forEach(element => {
+						this.states.push(element);
+					});
+					console.log(this.states + " Pacientes");
+				}
+			},
+			filterStates(){
+                if(this.nombre.length === 0){
+                    this.filteredStates = []
+                }else{
+                    this.filteredStates = this.states.filter(state => {
+                        return state.toLowerCase().includes(this.nombre.toLowerCase())
+                    })
+                }
+            },
+			async recon(){
+				if(this.nombre.length !== 0){
+					this.states.length = 0;
+					this.filteredStates.length = 0;
+					if(this.tipoBusqueda == "Pr"){
+						const res = await consultarProtocolosRex(this.nombre);
+						this.protocolos = res.data;
+						this.protocolos.forEach(element => {
+							this.states.push(element);
+						});
+						console.log(this.states + " Protocolos");
+					} else if(this.tipoBusqueda == "Pa") {
+						const res = await consultarPacientesRex(this.nombre);
+						this.pacientes = res.data;
+						this.pacientes.forEach(element => {
+							this.states.push(element);
+						});
+						console.log(this.states + " Pacientes");
+					}
+					this.filterStates()
+				}
+			},
+			periodChanged() {
+				// range, eventSource) {
+				// Demo does nothing with this information, just including the method to demonstrate how
+				// you can listen for changes to the displayed range and react to them (by loading items, etc.)
+				//console.log(eventSource)
+				//console.log(range)
+			},
+			thisMonth(d, h, m) {
+				const t = new Date()
+				/*console.log("AQUI ESTAS LO QUE BUSCAS")
+				console.log(t.getFullYear())
+				console.log(t.getMonth())
+				console.log(d)
+				console.log(h)
+				console.log(0, m)
+				console.log(0)
+				console.log("AQUI ESTAS LO QUE BUSCAS")*/
+				return new Date(t.getFullYear(), t.getMonth(), d, h || 0, m || 0)
+				
+			},
+			onClickDay(d) {
+				this.selectionStart = null
+				this.selectionEnd = null
+				this.message = `Dia seleccionado: ${d.toLocaleDateString()}`
+			},
+			onClickItem(e) {
+				this.message = `Evento seleccionado: ${e.title}`
+			},
+			setShowDate(d) {
+				this.message = `Haz cambiado la vista a ${d.toLocaleDateString()}`
+				this.showDate = d
+			},
+			setSelection(dateRange) {
+				this.selectionEnd = dateRange[1]
+				this.selectionStart = dateRange[0]
+			},
+			finishSelection(dateRange) {
+				this.setSelection(dateRange)
+				this.message = `Haz seleccionado: ${this.selectionStart.toLocaleDateString()} -${this.selectionEnd.toLocaleDateString()}`
+			},
+			onDrop(item, date) {
+				this.message = `Se ha cambiado ${item.id} a ${date.toLocaleDateString()}`
+				// Determine the delta between the old start date and the date chosen,
+				// and apply that delta to both the start and end date to move the item.
+				const eLength = CalendarMath.dayDiff(item.startDate, date)
+				item.originalItem.startDate = CalendarMath.addDays(item.startDate, eLength)
+				item.originalItem.endDate = CalendarMath.addDays(item.endDate, eLength)
+			},
+			clickTestAddItem() {
+				this.items.push({
+					startDate: this.newItemStartDate,
+					endDate: this.newItemEndDate,
+					title: this.newItemTitle,
+					id: "e" + Math.random().toString(36).substr(2, 10),
+				})
+				this.message = "Evento agregado con exito!!!"
+			},
 		},
-		setShowDate(d) {
-			this.message = `Haz cambiado la vista a ${d.toLocaleDateString()}`
-			this.showDate = d
-		},
-		setSelection(dateRange) {
-			this.selectionEnd = dateRange[1]
-			this.selectionStart = dateRange[0]
-		},
-		finishSelection(dateRange) {
-			this.setSelection(dateRange)
-			this.message = `Haz seleccionado: ${this.selectionStart.toLocaleDateString()} -${this.selectionEnd.toLocaleDateString()}`
-		},
-		onDrop(item, date) {
-			this.message = `Se ha cambiado ${item.id} a ${date.toLocaleDateString()}`
-			// Determine the delta between the old start date and the date chosen,
-			// and apply that delta to both the start and end date to move the item.
-			const eLength = CalendarMath.dayDiff(item.startDate, date)
-			item.originalItem.startDate = CalendarMath.addDays(item.startDate, eLength)
-			item.originalItem.endDate = CalendarMath.addDays(item.endDate, eLength)
-		},
-		clickTestAddItem() {
-			this.items.push({
-				startDate: this.newItemStartDate,
-				endDate: this.newItemEndDate,
-				title: this.newItemTitle,
-				id: "e" + Math.random().toString(36).substr(2, 10),
-			})
-			this.message = "Evento agregado con exito!!!"
-		},
-	},
-}
+	}
 </script>
 
 <style scoped>
